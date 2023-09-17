17eba5b6d62ae2e1bfbbabe70870660e534a9e4d
Michael Parchment <85087548+mparchment@users.noreply.github.com>
2023-09-17 18:35:12 -0400

    Implement new calendar

---
+++ src/components/WeeklyCalendar.jsx
@@ -0,0 +1,114 @@
+import styled from 'styled-components';
+import { addDays } from 'date-fns';
+import PropTypes from 'prop-types';
+
+const AlignCenter = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Wrapper = styled.div`
+    width: 70%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: stretch;
+  margin-bottom: 50px;
+  gap: 5px;
+
+  @media (max-width: 1366px) {
+    flex-direction: column;
+  }
+`;
+
+const DayColumn = styled.div`
+  flex: 1;
+  text-align: center;
+  min-width: calc(100% / 7);
+`;
+
+const DayTitle = styled.h3`
+  text-transform: uppercase;
+  margin-bottom: 5px;
+`;
+
+const Event = styled.div`
+  margin: 5px 0;
+  padding: 5px;
+  border-radius: 5px;
+  background: ${({ bgColor }) => `#${bgColor}`};
+  font-weight: 600;
+  box-shadow: 0px 3px 0 rgba(0, 0, 0, 0.1);
+  padding: 10px;
+  font-size: 14px;
+`;
+
+
+const EventsWrapper = styled.div`
+    border-radius: 5px;
+    padding: 10px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    gap: 2px;
+`;
+
+const WeeklyCalendar = ({ events }) => {
+  const today = new Date();
+  const daysOfWeek = Array.from({ length: 7 }, (_, i) => addDays(today, i));
+  const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const colorPalette = [
+    '#fff0e0',
+    '#fce0cc',
+  ];
+  
+
+  const eventsWithColors = events.map((event, index) => {
+    return {
+      ...event,
+      color: colorPalette[index % colorPalette.length],
+    };
+  });
+
+  const formatTime = (date) => {
+    const hours = date.getHours();
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+  
+    return `${(hours + 11) % 12 + 1}:${minutes} ${ampm}`;
+  };  
+
+  return (
+    <AlignCenter>
+      <Wrapper>
+        {daysOfWeek.map((day, index) => {
+          const dayEvents = eventsWithColors.filter((event) =>
+            day.toISOString().split('T')[0] === new Date(event.date).toISOString().split('T')[0]
+          );
+
+          dayEvents.sort((a, b) => new Date(a.startTime) - new Date(b.startTime));
+
+          return (
+            <DayColumn key={index}>
+              <DayTitle>{dayNames[day.getDay()]}</DayTitle>
+              <EventsWrapper>
+                {dayEvents.map((event, i) => (
+                  <Event key={i} style={{backgroundColor: `${event.color}`}}>
+                    <strong>{event.name}</strong><br/>{formatTime(new Date(event.startTime))}
+                  </Event>
+                ))}
+              </EventsWrapper>
+            </DayColumn>
+          );
+        })}
+      </Wrapper>
+    </AlignCenter>
+  );
+};
+
+WeeklyCalendar.propTypes = {
+    events: PropTypes.array.isRequired,
+};
+
+export default WeeklyCalendar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,8 @@ import useIsMobile from "../hooks/useIsMobile";
 
 import ProgramCard from "../components/ProgramCard";
 import ServiceCard from "../components/ServiceCard";
-import Calendar from "../components/Calendar";
+import OldCalendar from "../components/Calendar";
+import WeeklyCalendar from "../components/WeeklyCalendar";
 
 import MCCFront from "../assets/mcc-front.jpg";
 import ShaykhAbdussamadPortrait from "../assets/shaykh-abdussamad-portrait.png";
@@ -61,6 +62,24 @@ function stripHtml(html) {
   return doc.body.textContent || "";
 }
 
+const events = [
+  // Existing events
+  { name: 'Fajr Halaqah', date: '2023-09-18', startTime: '2023-09-18T05:30:00' },
+  { name: 'Sirah Study', date: '2023-09-18', startTime: '2023-09-18T19:30:00' },
+  { name: 'Tazkiyah Tuesdays', date: '2023-09-19', startTime: '2023-09-19T07:30:00' },
+  { name: 'Hadith Study', date: '2023-09-20', startTime: '2023-09-20T19:30:00' },
+  { name: 'YAP Book Club', date: '2023-09-21', startTime: '2023-09-21T07:30:00' },
+  { name: "1st Jumu'ah", date: '2023-09-22', startTime: '2023-09-22T13:00:00' },
+  { name: "2nd Jumu'ah", date: '2023-09-22', startTime: '2023-09-22T14:00:00' },
+  { name: 'MYO Halaqah', date: '2023-09-22', startTime: '2023-09-22T18:00:00' },
+  { name: 'Breakfast Club', date: '2023-09-23', startTime: '2023-09-23T09:00:00' },
+  { name: 'MCC Cleanup', date: '2023-09-23', startTime: '2023-09-23T11:00:00' },
+  { name: 'Family Picnic', date: '2023-09-17', startTime: '2023-09-17T12:00:00' },
+  ...['2023-09-17', '2023-09-18', '2023-09-19', '2023-09-20', '2023-09-21'].map(date => [
+    { name: 'Islamic Studies', date, startTime: `${date}T16:00:00` },
+    { name: "Qur'an Program", date, startTime: `${date}T17:00:00` },
+  ]).flat(),
+];
 function Home() {
   const isMobile = useIsMobile();
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -246,7 +265,7 @@ function Home() {
           </IntroductionCard>
         </IntroductionWrapper>
       )}
-      {/*<Calendar />*/}
+      {/*<OldCalendar />*/}
       <ProgramsSection>
         <TitleWrapper>
           <ProgramTitle>Programs</ProgramTitle>
@@ -279,6 +298,11 @@ function Home() {
           />
         </ProgramsWrapper>
       </ProgramsSection>
+      <TitleWrapper>
+        <ProgramTitle>Calendar</ProgramTitle>
+        <Divider />
+      </TitleWrapper>
+      <WeeklyCalendar events={events}/>
       <TitleWrapper>
         <ProgramTitle>Services</ProgramTitle>
         <Divider />
